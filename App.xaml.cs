9d6d0a5526c11c0ebdb10703040b3cfa1d94e8d1
wsong2 <weixus@hotmail.com>
2015-11-06 09:34:48 +0000

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+using System;
+//using System.Globalization;
+using System.Windows;
+//using System.Threading;
+
+namespace AccGrid
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+		//Thread.CurrentThread.CurrentUICulture = new CultureInfo("zh-CN");
+		//Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("zh-CN");
+        //Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("zh-CN");
+    }
+}
